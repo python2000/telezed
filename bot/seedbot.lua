e592445779d6a9f2af8f33fff5a937ea30577a00
TeleAgentRobot <kingagent47.ar@gmail.com>
2016-02-13 19:24:00 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -229,26 +229,12 @@ function create_config( )
     sudo_users = {98962756},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v2 - Open Source
-An advance Administration bot based on yagop/telegram-bot 
-
-https://github.com/SEEDTEAM/TeleSeed
-
+    about_text = [[TeleAgent
+An advance Administration bot
 Admins
-@iwals [Founder]
-@imandaneshi [Developer]
-@Rondoozle [Developer]
-@seyedan25 [Manager]
-
-Special thanks to
-awkward_potato
-Siyanew
-topkecleon
-Vamptacus
-
-Our channels
-@teleseedch [English]
-@iranseed [persian]
+@XHACKERX [manager,developer,etc]
+Our channel
+@TeleAgentTeam [persian]
 ]],
     help_text_realm = [[
 Realm Commands:

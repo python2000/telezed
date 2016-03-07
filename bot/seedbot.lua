081fbe099c4ed6b6894e6bf849d3ebb7333e9b48
python2000 <ptey95@gmail.com>
2016-03-07 12:39:36 +0330

    seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {98962756},--Sudo users
+    sudo_users = {147476153},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
